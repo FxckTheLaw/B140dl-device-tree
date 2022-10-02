f3629c74fb1faf0c0cfc2da9bbae5ef2e577e2d2
oakieville <oakieville209@gmail.com>
2022-10-02 11:07:41 -0500

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2019-Present A-Team Digital Solutions
+#
+
+PRODUCT_MAKEFILES := \
+     $(LOCAL_DIR)/omni_B140DL.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_B140DL-user \
+    omni_B140DL-userdebug \
+    omni_B140DL-eng
