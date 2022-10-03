fcc5c51ebc4d75ccacd5c0e7c9dda5a818dd2db2
oakieville <oakieville209@gmail.com>
2022-10-03 08:01:24 -0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,23 +1,10 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Replace $$DEVICE$$ with your Device Name's Value. Mine is Primo_RX5.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value, Mine is WALTON 
 
-LOCAL_PATH := $(call my-dir)
+ifneq ($(filter $$DEVICE$$,$(TARGET_DEVICE)),)
 
-ifeq ($(TARGET_DEVICE), B140DL)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+LOCAL_PATH := device/$$BRAND$$/$$DEVICE$$
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
 endif
